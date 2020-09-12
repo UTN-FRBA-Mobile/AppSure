49a71f16dc2e036380b8241b7dc797298bb6aaf4
Ariel Ventrice <goska2732@gmail.com>
2020-09-12 01:54:32 -0300

    hello world with navigation component and main fragment

--- app/src/main/java/com/utn/appsure/activity/MainActivity.kt
+++ app/src/main/java/com/utn/appsure/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.utn.appsure
+package com.utn.appsure.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.utn.appsure.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/utn/appsure/fragment/MainListFragment.kt
@@ -0,0 +1,15 @@
+package com.utn.appsure.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.utn.appsure.R
+
+class MainListFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main_list, container, false)
+    }
+}
