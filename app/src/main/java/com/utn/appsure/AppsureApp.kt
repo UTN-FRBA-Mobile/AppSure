a915b60bb57e63d42f6531675eb625522ec0d56c
Ariel Ventrice <goska2732@gmail.com>
2020-09-12 12:20:08 -0300

    data module

--- app/src/main/java/com/utn/appsure/AppsureApp.kt
+++ app/src/main/java/com/utn/appsure/AppsureApp.kt
@@ -2,6 +2,7 @@ package com.utn.appsure
 
 import android.app.Application
 import com.utn.appsure.di.appModule
+import com.utn.data.di.dataModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -10,7 +11,7 @@ class AppsureApp : Application() {
         super.onCreate()
         startKoin {
             androidContext(this@AppsureApp)
-            modules(appModule)
+            modules(listOf(dataModule, appModule))
         }
     }
 }
---
+++ data/src/main/java/com/utn/data/di/DataModule.kt
@@ -0,0 +1,5 @@
+package com.utn.data.di
+
+import org.koin.dsl.module
+
+val dataModule = module {  }
