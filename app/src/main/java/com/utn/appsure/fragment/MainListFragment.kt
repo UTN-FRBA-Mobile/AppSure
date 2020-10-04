e9773a9cafa9806b88734f245b6b3465b826be44
Ariel Ventrice <goska2732@gmail.com>
2020-10-04 18:54:05 -0300

    changed navigation for policy activity

--- app/src/main/java/com/utn/appsure/fragment/MainListFragment.kt
+++ app/src/main/java/com/utn/appsure/fragment/MainListFragment.kt
@@ -1,12 +1,15 @@
 package com.utn.appsure.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.utn.appsure.activity.PolicyActivity
 import com.utn.appsure.databinding.FragmentMainListBinding
 import com.utn.appsure.viewmodel.MainListViewModel
+import kotlinx.android.synthetic.main.fragment_main_list.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainListFragment : Fragment() {
@@ -24,5 +27,17 @@ class MainListFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewmodel = viewModel
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        create_policy_fab.setOnClickListener {
+            startActivity(
+                Intent(
+                    activity,
+                    PolicyActivity::class.java
+                )
+            )
+        }
     }
 }
