bb8839b5295f7f8c954f7685b1080a0efdeb5e80
Ariel Ventrice <goska2732@gmail.com>
2020-10-31 23:46:51 -0300

    merge fix

--- app/src/main/java/com/utn/appsure/di/AppModule.kt
+++ app/src/main/java/com/utn/appsure/di/AppModule.kt
@@ -2,10 +2,7 @@ package com.utn.appsure.di
 
 import com.utn.appsure.service.PruebaService
 import com.utn.appsure.usecase.PruebaUseCase
-import com.utn.appsure.viewmodel.CreatePolicyViewModel
-import com.utn.appsure.viewmodel.CreatePolicyViewModel2
-import com.utn.appsure.viewmodel.InvoiceViewModel
-import com.utn.appsure.viewmodel.MainListViewModel
+import com.utn.appsure.viewmodel.*
 import okhttp3.OkHttpClient
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
