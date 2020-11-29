a9444e7bfa5dba6e8d6086d866ca9b6a04969ef4
Alejandro <mazzeo.alejandro@gmail.com>
2020-11-29 16:03:45 -0300

    asignacion de variables para layout PolicyDetail

--- app/src/main/java/com/utn/appsure/viewmodel/PolicyDetailViewModel.kt
+++ app/src/main/java/com/utn/appsure/viewmodel/PolicyDetailViewModel.kt
@@ -1,28 +1,25 @@
 package com.utn.appsure.viewmodel
 
 import androidx.databinding.ObservableField
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.utn.appsure.model.Policy
 import com.utn.appsure.usecase.GetPolicyUseCase
 
 class PolicyDetailViewModel(private var policyUseCase: GetPolicyUseCase) : ViewModel() {
     var patent = ObservableField<String>()
-    val brand = ObservableField<String>()
-    val model = ObservableField<String>()
-    val year = ObservableField<String>()
-    val colour = ObservableField<String>()
+    var brand = ObservableField<String>()
+    var model = ObservableField<String>()
+    var year = ObservableField<String>()
+    var colour = ObservableField<String>()
 
     fun getPolicy(policyLicenseID: String) {
         policyUseCase.execute(policyLicenseID) {
-            Policy(
-                patent.get() ?: it?.license.toString(),
-                brand.get() ?: it?.brand.toString(),
-                model.get() ?: it?.model.toString(),
-                Integer.parseInt(year.get() ?: "2020"),
-                colour.get() ?: it?.colour.toString(), 0, 0.0, 0.0
-            )
-            patent = ObservableField("TEST")
+            patent = ObservableField(it?.license.toString())
+            brand = ObservableField(it?.brand.toString())
+            model = ObservableField(it?.model.toString())
+            year = ObservableField(it?.year.toString())
+            colour = ObservableField(it?.colour.toString())
+
         }
 
     }
